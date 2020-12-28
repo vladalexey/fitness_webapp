9349019bf5662321a2341f3b33c593e263499c5f
Thao Phan <qmp23@drexel.edu>
2020-12-27 22:37:07 -0500

    Add appbar buttons

--- lib/screens/home/components/app_bar.dart
+++ lib/screens/home/components/app_bar.dart
@@ -6,16 +6,73 @@ class CustomAppBar extends StatelessWidget {
     return Container(
       margin: EdgeInsets.all(20.0),
       padding: EdgeInsets.all(20.0),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(40.0),
-          boxShadow: [
-            BoxShadow(
-              offset: Offset(0, -2),
-              blurRadius: 30.0,
-              color: Colors.black.withOpacity(0.15),
-            )
-          ]),
+      // decoration: BoxDecoration(
+      //     color: Colors.white,
+      //     borderRadius: BorderRadius.circular(40.0),
+      //     boxShadow: [
+      //       BoxShadow(
+      //         offset: Offset(0, -2),
+      //         blurRadius: 30.0,
+      //         color: Colors.black.withOpacity(0.15),
+      //       )
+      //     ]),
+      child: Positioned(
+        right: 30.0,
+        child: Padding(
+          padding:
+              EdgeInsets.only(left: MediaQuery.of(context).size.width / 5 * 3),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              InkWell(
+                onTap: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    "Workout",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 5.0,
+              ),
+              InkWell(
+                onTap: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    "Nutrition",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 5.0,
+              ),
+              InkWell(
+                onTap: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    "About us",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
