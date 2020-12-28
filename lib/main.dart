f7948eadc1e32cd1d4bfb175a25c0129f3158a06
Thao Phan <qmp23@drexel.edu>
2020-12-27 22:36:13 -0500

    Change web app name

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pump It Up',
       theme: ThemeData(
         primaryColor: kPrimaryColor,
       ),
