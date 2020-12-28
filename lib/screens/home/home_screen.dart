3d3b170af9aedb170b672715a63265892063d663
Thao Phan <qmp23@drexel.edu>
2020-12-27 22:38:15 -0500

    Update to scaffold and add components to homescreen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:fitness_webapp/screens/home/components/app_bar.dart';
+import 'package:fitness_webapp/screens/home/components/body.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,15 +7,17 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Container(
-      height: size.height,
-      width: size.width,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-            image: AssetImage("assets/images/bg.png"), fit: BoxFit.cover),
-      ),
-      child: Column(
-        children: <Widget>[CustomAppBar()],
+    return Scaffold(
+      body: Container(
+        height: size.height,
+        width: size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/bg2.png"), fit: BoxFit.cover),
+        ),
+        child: Column(
+          children: <Widget>[CustomAppBar(), Body()],
+        ),
       ),
     );
   }
